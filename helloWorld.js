177eaeb5f3a07037b0157e8dc40755f62a244800
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-11-12 11:32:22 -0500

    Testing node commands in terminal

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
+/*
+the above shebang line is used to tell the OS which interpreter to use for running this script file
+the shebang needs to be the first line in a file 
+*/
+
+/*
+to use a shebang, you need to give the file executable permission.
+to do this, you can use the following terminal command (make sure you are in the same directory with the file):
+chmod u+x helloWorld.js
+*/
 
 console.log("Hello from Node!");
+
+/* 
+use the following command in the terminal to run this script
+node helloWorld.js
+*/
