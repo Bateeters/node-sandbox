6223a8bb17f24aa2baf5f501fb77cdc407157317
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-11-16 14:54:43 -0500

    added basic POST request using axios

---
+++ axiosPostRequest.js
@@ -0,0 +1,20 @@
+// Look at axiosGetRequest.js file for full comment notes.
+
+const axios = require ('axios');
+
+axios
+    // the URL we are sending the data to
+    .post('https://jsonplaceholder.typicode.com/posts', {
+        // the body (payload) of the POST request
+        title: 'Hello World',
+        body: 'This is a test POST request',
+        userId: 13
+    })
+    .then(res => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res.data);  // using res.data to only return the object's data rather than the ENTIRE object
+                                // remove ".data" to see what the whole object looks like (it's A LOT).
+    })
+    .catch(error => {
+        console.error(error)
+    });
